ad8a9bfb9ea8ddeff5e768c8504f12619ceedefd
Granted <anjishnu.04032007@gmail.com>
2024-02-25 02:52:21 +0530

    modified:   src/app/dashboard/page.tsx 	modified:   src/middleware.ts 	deleted:    src/validate.ts 	modified:   tsconfig.json

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,8 @@
 "use client"
 
-import { sendAuth } from '@/validate';
 import React from 'react';
 
 export default function dashboardPage() {
-    sendAuth();
     return (
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <p className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,21 @@
 
+import axios from 'axios';
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
+import { cookies } from "next/headers";
  
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
     if (!request.cookies.has('Auth-Token')) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
+    const auth = cookies().get('Auth-Token');
+    let api_endpoint = process.env.NEXT_PUBLIC_API_URL?.concat("validate");
+    axios
+        .post(api_endpoint! , auth)
+        .catch(() => {
+            return NextResponse.redirect(new URL('/login', request.url))
+        })
 }
 
 export const config = {
