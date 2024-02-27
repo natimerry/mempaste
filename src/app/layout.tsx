8251d5ae9f0dd3fc23aedd7fcae3b5e4087ae689
Soham Nandy <soham.nandy2006@gmail.com>
2024-02-27 12:14:59 +0530

    added toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         <section className="bg-gray-50  dark:bg-zinc-950 h-screen">
           {children}
         </section>
+        <Toaster/>
       </body>
     </html>
   );
