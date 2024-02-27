4573af013bf327fd32c4dffe4d48bc0b2f5e4009
Soham Nandy <soham.nandy2006@gmail.com>
2024-02-27 12:27:37 +0530

    Update container stucture

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -8,9 +8,7 @@ export default function Container({children}: IProps) {
 
     return (
         <div className="w-full bg-gray-300 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 ring-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8 dark:bg-zinc-900 bg-slate-50 rounded-lg border-2  border-gray-700 dark:border-white">
                 {children}
-            </div>
         </div>
 
     );
---
+++ src/app/components/DialogContainer.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useState } from 'react';
+interface IProps {
+    children: React.ReactNode;
+}
+export default function Dialog({ children }: IProps) {
+
+    return (
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8 dark:bg-zinc-900 bg-slate-50 rounded-lg border-2  border-gray-700 dark:border-white">
+            {children}
+        </div>
+
+    );
+}
