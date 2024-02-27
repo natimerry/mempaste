d9dcaf2531b3d0c4e4b08aa478bdfa28e667c7a7
Soham Nandy <soham.nandy2006@gmail.com>
2024-02-27 12:13:25 +0530

    Create container components

---
+++ src/app/components/ParentDiv.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useState } from 'react';
+interface IProps {
+  children: React.ReactNode;
+}
+export default function RootContainer({children}: IProps) {
+  return (
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        {children}
+    </div>
+
+  );
+}
