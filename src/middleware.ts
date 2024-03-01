e09c364861d28c60ca4b07896ae3138d97f7e34c
Soham Nandy <soham.nandy2006@gmail.com>
2024-03-01 12:45:58 +0530

    fix middleware not redirecting on wrong cookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,18 +6,25 @@ import { cookies } from "next/headers";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
+    let is_cookie_valid: boolean = false;
     if (!request.cookies.has('Auth-Token')) {
         console.log("COOKIE NOT FOUND")
-        return NextResponse.redirect(new URL('/', request.url))
+        is_cookie_valid=false;
     }
     const auth = cookies().get('Auth-Token');
+    console.log(auth);
     let api_endpoint = process.env.NEXT_PUBLIC_API_URL?.concat("validate");
     axios
         .post(api_endpoint!, auth)
         .catch(() => {
-            console.log("COOKIE INVALID")
-            return NextResponse.redirect(new URL('/', request.url))
+            console.log("COOKIE INVALID");
+            is_cookie_valid=false;
         })
+    
+    if (!is_cookie_valid){
+        return NextResponse.redirect(new URL('/', request.url))
+
+    }
 }
 
 export const config = {
